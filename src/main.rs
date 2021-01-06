d998877b256ae0ed1b16b0b850b9d0ea68935370
Nicolas Zordan <nicolas.zordan.tek@gmail.com>
2021-01-06 16:26:16 +0100

    added some dummy routes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello World!"
+// start a simc from the simc string
+#[post("/sim", data = "<simc_string>")]
+fn sim(simc_string: String) -> &'static str {
+    "sim_simc_string not implemented"
+}
+
+// gets the version of the current installed simc
+#[get("/simc/version")]
+fn simc_version() -> &'static str {
+    "simc_version not implemented"
+}
+
+// upgrade the currently installed simc to the latest available version
+#[get("/simc/upgrade")]
+fn simc_upgrade() -> &'static str {
+    "simc_upgrade not implemented"
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![sim, simc_version, simc_upgrade])
+        .launch();
 }
+
+#[cfg(test)]
+mod tests;
---
+++ src/tests.rs
@@ -0,0 +1,17 @@
+use rocket::http::Status;
+use rocket::local::Client;
+
+#[test]
+fn test_sim() {
+    unimplemented!();
+}
+
+#[test]
+fn test_simc_version() {
+    unimplemented!();
+}
+
+#[test]
+fn test_simc_upgrade() {
+    unimplemented!();
+}
